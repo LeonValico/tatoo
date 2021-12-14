d05035b7826e504c9fe0b5c58f90519b43a3eb9a
LeonValico <92885068+LeonValico@users.noreply.github.com>
2021-12-14 14:15:02 +0200

    v1

---
+++ js/init.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+$(document).ready(function(){
+      $('.modal').modal();
+    });
+
+$(document).ready(function() {
+        M.updateTextFields();
+      });
+
+$(document).ready(function(){
+      $('.slider').slider();
+    });
